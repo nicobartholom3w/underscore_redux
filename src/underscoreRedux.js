6a9e2e78abf309775d25230adca6c8646ef8d9bb
Nicole Bartholomew <nabartholomew@gmail.com>
2019-01-10 19:26:41 -0600

    (feat) _.filter() initial solution completed.

--- src/underscoreRedux.js
+++ src/underscoreRedux.js
@@ -90,6 +90,17 @@
 
   // Return all elements of an array that pass a truth test.
   _.filter = function(collection, test) {
+    var newCollection = [];
+
+    for (var key = 0; key < collection.length; key++){
+      var num = collection[key];
+
+     if (test(num)) {
+        newCollection.push(num);
+      }    
+    }
+
+    return newCollection;
   };
 
   // Return all elements of an array that don't pass a truth test.
