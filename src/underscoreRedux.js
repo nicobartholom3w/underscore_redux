87f25a240220023202f70ab494dd74670b55b455
Nicole Bartholomew <nabartholomew@gmail.com>
2019-01-30 17:05:19 -0600

    (feat) Completed filter and reject exercises.

--- src/underscoreRedux.js
+++ src/underscoreRedux.js
@@ -99,14 +99,57 @@
     //   }    
     // }
     // return newCollection;
+    let newCollection = [];
+  
+    _.each(collection, (item) => {
+      if(test(item)){
+        newCollection.push(item);
+      }
+    });
 
-    _.each(collection, test(item, i)) 
+    // let boundFunc = function thing(){
+
+    // }.bind(this);
+
+    return newCollection;
   };
+
+  // _.filter(function(){
+  //   this.blah
+  // })
+
+  // _.blah
+
+  // function _() {
+
+  // }
+
+  // let obj = {};
+
+  // obj.filter = function() {
+
+  // }
+
+  // _.filter(function() {
+
+  // });
 
   // Return all elements of an array that don't pass a truth test.
   _.reject = function(collection, test) {
     // TIP: see if you can re-use _.filter() here, without simply
     // copying code in and modifying it
+    // 
+    // 
+    let rejects = _.filter(collection, (item) => {
+      if(!test(item)){
+        return true;
+      }
+      else {
+        return false;
+      }
+    });
+
+    return rejects;
   };
 
   // Produce a duplicate-free version of the array.
